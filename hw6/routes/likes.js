8205b8a5a19ae4a9dff78ce019990236b3ec2102
박세은 <pkse1213@naver.com>
2019-06-20 06:35:58 +0900

    좋아요 수정

--- hw6/routes/likes.js
+++ hw6/routes/likes.js
@@ -9,15 +9,28 @@ const moment = require('moment');
 const timeFormat = moment().format('YYYY-MM-DD HH:mm:ss');
 
 router.post('/', async (req, res)=>{
-    let likeQuery = 'INSERT INTO `like` (`webtoonIdx`, `userIdx`) VALUES (?,?)'
-    let result = await pool.queryParam_Arr(likeQuery, [req.body.webtoonIdx, req.body.userIdx]);
-    console.log(result);
-    if(!result) {
-        res.status(200).send(authUtil.successFalse(statusCode.BAD_REQUEST, resMsg.LIKE_FAIL));
+    let checkLikeQuery = 'SELECT userIdx FROM `like` WHERE `webtoonIdx` = ?, `userIdx` = ?';
+    let addLikeQuery = 'INSERT INTO `like` (`webtoonIdx`, `userIdx`) VALUES (?,?)'
+    let cancelLikeQuery = 'DELETE FROM `like` WHERE `webtoonIdx` = ?, `userIdx` = ?';
+    let chekResult = await pool.queryParam_Arr(checkLikeQuery, [req.body.webtoonIdx, req.body.userIdx]);
+    console.log(chekResult);
+    
+    if(chekResult.length==0) {
+        let result = await pool.queryParam_Arr(addLikeQuery, [req.body.webtoonIdx, req.body.userIdx]);
+        if(!result){
+            res.status(200).send(authUtil.successFalse(statusCode.BAD_REQUEST, resMsg.ADD_LIKE_FAIL));
+        } else {
+            res.status(200).send(authUtil.successTrue(statusCode.OK, resMsg.ADD_LIKE_SUCCESS));
+        }
+    } else if (chekResult.length>0) {
+        let result = await pool.queryParam_Arr(cancelLikeQuery, [req.body.webtoonIdx, req.body.userIdx]);
+        if(!result){
+            res.status(200).send(authUtil.successFalse(statusCode.BAD_REQUEST, resMsg.CANCEL_LIKE_FAIL));
+        } else {
+            res.status(200).send(authUtil.successTrue(statusCode.OK, resMsg.CANCEL_LIKE_SUCCESS));
+        }
     }
-    else {
-        res.status(200).send(authUtil.successTrue(statusCode.OK, resMsg.LIKE_SUCCESS));
-    }
+    
 })
 
 router.get('/:webtoonIdx', async (req, res)=>{
