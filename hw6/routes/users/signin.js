ca454c16075080cdc595a6541f0a1b4f2ed59068
SimJungUk <simju9397@gmail.com>
2019-06-22 04:56:22 +0900

    [변수명 수정 최종본]

--- hw6/routes/users/signin.js
+++ hw6/routes/users/signin.js
@@ -12,7 +12,7 @@ const loginVerify = require('../../modules/loginVerify')
 //로그인
 router.post('/', async (req, res) => {
     const getuserQuery = 'SELECT * FROM user WHERE id = ?';
-    const firstTokenQuery = 'UPDATE user SET refreshToken=? WHERE id=?'
+    const updateRefreshTokenQuery = 'UPDATE user SET refreshToken=? WHERE id=?'
     const getUserResult = (await pool.queryParam_Parse(getuserQuery, [req.body.id]))[0];
     if (!getUserResult || getUserResult.length == 0) {
         res.status(200).send(authUtil.successFalse(statusCode.DB_ERROR, resMessage.ID_OR_PASSWORD_INCORRECT));
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
             if(getUserResult.refreshToken == '' || refreshVerify == -3) { //첫 로그인이라 아예 리프레쉬토큰이 없을 때 or refreshToken 이 만료됐을 때
                 console.log('First login. Token complete.');
                 const tokens = await jwtUtils.sign(req.body);
-                await pool.queryParam_Arr(firstTokenQuery, [tokens.refreshToken, req.body.id]);
+                await pool.queryParam_Arr(updateRefreshTokenQuery, [tokens.refreshToken, req.body.id]);
                 res.status(200).send(authUtil.successTrue(statusCode.OK, resMessage.SIGNIN_SUCCESS, tokens));
             }
             else if (accessVerify == -3 || accessVerify == -2){ //verify 결과 유효하지 않거나 만료된 accessToken 일때
